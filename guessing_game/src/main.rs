c6626524048ed52a240a65749cc163eb1d20fa01
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-10 12:21:18 -0500

    updated guessing game with computer guess.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,20 @@
 use std::io;
+use rand::prelude::*;
 
 fn main() {
+    // Prompt user for input
     println!("Guess the number!");
     println!("Please input your guess.");
 
+    // Setup mutable variable to store user input then store it
     let mut guess = String::new(); // Is this the same as var = "" in other languages
-
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+    // Generate a random number between 1 and 100
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+
+    // Print out the secret and user input
+    println!("The secret number is: {secret_number}");
     println!("You guessed: {}", guess);
 }
