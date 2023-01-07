62f0f6df10d397753de434ce4b52e99bfd1c0f00
Stephen Akuffo <sa.akuffo@gmail.com>
2023-01-07 14:15:44 -0500

    Struct v1

--- guessing_gamev2/src/main.rs
+++ guessing_gamev2/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     // Prompt user for input
@@ -12,21 +12,22 @@ fn main() {
 
         let mut guess = String::new(); // Is this the same as var = "" in other languages
 
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse(){
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        match guess.cmp(&secret_number){
-            
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win! {} guesses", counter);
+                println!("You win!");
                 break;
-            }        }
+            }
+        }
     }
-
 }
