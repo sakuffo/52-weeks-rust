38b2e3a349f25204796cb6e611d4571f7d70a91e
Stephen Akuffo <sa.akuffo@gmail.com>
2023-01-21 13:23:58 -0500

    coin enum co pilot app

--- ep8/src/main.rs
+++ ep8/src/main.rs
@@ -1,3 +1,30 @@
+// build a coin
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+// create a function that matches the coin and returns a numeric value
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+// Call the function and print the result for each coin
 fn main() {
-    println!("Hello, world!");
+    let penny = Coin::Penny;
+    let nickel = Coin::Nickel;
+    let dime = Coin::Dime;
+    let quarter = Coin::Quarter;
+
+    println!("Penny: {}", value_in_cents(penny));
+    println!("Nickel: {}", value_in_cents(nickel));
+    println!("Dime: {}", value_in_cents(dime));
+    println!("Quarter: {}", value_in_cents(quarter));
 }
