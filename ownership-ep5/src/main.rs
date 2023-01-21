8f4f3c5f6e600a711755c9c77a14875e89da5f4d
Stephen Akuffo <sa.akuffo@gmail.com>
2023-01-21 13:17:21 -0500

    check

--- ownership-ep5/src/main.rs
+++ ownership-ep5/src/main.rs
@@ -6,14 +6,28 @@ struct Rectangle {
 
 impl Rectangle {
     fn area(&self) -> u32 {
-        println!("Calculating area of rectangle...{}", self.width)
+        println!("Calculating area of rectangle...{}", self.width);
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
 fn main() {
     let rect1 = Rectangle {
         width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 600,
         height: 50,
     };
 
